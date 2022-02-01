ab8d915a79bb25a52ecd32d5deebc16570da29ea
Rodrigo Zuanny <rozuanny@gmail.com>
2022-01-31 23:38:14 -0300

    correcoes feitas na calculadora IMC e Casos de Covid

--- CalculadoraIMC.js
+++ CalculadoraIMC.js
@@ -2,3 +2,4 @@ let altura = 1.85
 let peso = 87
 
 let IMC = peso / altura ** 2
+console.log(`Seu Imc e: ${IMC}`)
--- CasosCovid19.js
+++ CasosCovid19.js
@@ -2,4 +2,6 @@ let po = 1000
 let t = 100
 let x = 4
 let P = po * Math.pow(x, t / 7)
-console.log(P)
+console.log(
+  P + `Sera o numero de pessoas infectadas apos ${t} tempos percorrido`
+)
