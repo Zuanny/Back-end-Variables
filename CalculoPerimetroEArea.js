27184f27ec91926dcab9e5d8702eecf967b39062
Rodrigo Zuanny <rozuanny@gmail.com>
2022-01-31 22:40:34 -0300

    Calculo  da area e perimetro realizado

---
+++ CalculoPerimetroEArea.js
@@ -0,0 +1,7 @@
+let diametro = 3.18
+let perimetro = diametro * Math.PI
+let area = (Math.pow(diametro, 2) * Math.PI) / 4
+
+console.log(
+  `Para um diametro de: ${diametro} o seu perimetro e: ${perimetro} e a sua Area e igual a: ${area}`
+)
