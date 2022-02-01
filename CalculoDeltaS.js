1d4278d2c91ce9c0386b1a86316e91fa8db2a562
Rodrigo Zuanny <rozuanny@gmail.com>
2022-01-31 21:41:14 -0300

    Exercicio 5 e 6 realizados

---
+++ CalculoDeltaS.js
@@ -0,0 +1,4 @@
+let pontoUm = 10
+let pontoDois = 55
+let deltaS = pontoDois - pontoUm
+console.log(`a distancia e igual a ${deltaS}`)
---
+++ VelocidadeMedia.js
@@ -0,0 +1,4 @@
+let espacoMetro = 50
+let tempoSegundos = 150
+let velocidadeMxS = espacoMetro / tempoSegundos
+console.log(velocidadeMxS)
